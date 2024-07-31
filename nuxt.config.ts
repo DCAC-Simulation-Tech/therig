b07fc5410f8cff838b707071fca2d5d8d7ce80d6
kamalson dasari <dasarikamalson@gmail.com>
2024-07-31 13:35:33 +0530

    First Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+  alias: {
+    '@' : resolve(__dirname, "/"),
+  },
+  css: ['~/assets/css/main.css',],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+     <h1 class="text-3xl font-bold uppercase text-center">Hello Riggers</h1>
+    </div>
+  </template>
+  
